d3a2f7990d6bbb43b872eaf96e7c59f8fef29e5c
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2021-06-19 13:26:32 +0900

    Initial Commit

---
+++ news-api/news_apiApp.swift
@@ -0,0 +1,17 @@
+//
+//  news_apiApp.swift
+//  news-api
+//
+//  Created by 萩倉丈 on 2021/06/19.
+//
+
+import SwiftUI
+
+@main
+struct news_apiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
